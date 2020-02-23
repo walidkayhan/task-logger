c7f0ac148e04663dcb6b7e005539fc85ae27eacd
Walid <walid.kayhan@mohawkcollege.ca>
2020-02-23 00:14:13 -0500

    Replaced let with const

--- models/User.js
+++ models/User.js
@@ -1,15 +1,15 @@
-let mongoose = require("mongoose");
-let Schema = mongoose.Schema;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-let UserSchema = new Schema({
+const UserSchema = new Schema({
   username: { type: String, required: true },
   password: { type: String, required: true },
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
-  emailAddress: { type: String, required: true },
+  email: { type: String, required: true },
   position: { type: String, required: true },
-  favoriteColor: { String, required: true },
+  favoriteColor: { type: String, required: true },
   created: { type: Date, default: Date.now }
 });
 
-export default UserModel = mongoose.model("UserModel", UserSchema);
+module.exports = mongoose.model("UserModel", UserSchema);
