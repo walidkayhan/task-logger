03534c8255edca3c07f6c04eea56980d92bfbce1
Walid <walid.kayhan@mohawkcollege.ca>
2020-02-23 00:14:01 -0500

    Replaced let with const

--- models/Task.js
+++ models/Task.js
@@ -1,7 +1,7 @@
-let mongoose = require("mongoose");
-let Schema = mongoose.Schema;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-let TaskSchema = new Schema({
+const TaskSchema = new Schema({
   title: { type: String, required: true },
   type: String,
   additionalInformation: String,
