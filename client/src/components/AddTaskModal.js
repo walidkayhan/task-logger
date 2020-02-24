2b69000f4aae9b17a3ce0559130dce34fac215ec
Walid <walid.kayhan@mohawkcollege.ca>
2020-02-24 08:11:22 -0500

    Implemented add task modal component

---
+++ client/src/components/AddTaskModal.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { Modal, Button } from "antd";
+
+const AddTaskModal = () => {
+  const [visible, setVisible] = useState(false);
+
+  const onOK = e => {
+    console.log(e);
+    setVisible(false);
+  };
+
+  const onCancel = e => {
+    console.log(e);
+    setVisible(false);
+  };
+
+  return (
+    <div>
+      <Button type="primary" onClick={() => setVisible(true)}>
+        Open Modal
+      </Button>
+      <Modal
+        title="Add New Task"
+        visible={visible}
+        onOk={onOK}
+        onCancel={onCancel}
+        footer={[
+          <Button key="back" onClick={onCancel}>
+            Cancel
+          </Button>,
+          <Button key="submit" type="primary" onClick={onOK}>
+            Add
+          </Button>
+        ]}
+      >
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Modal>
+    </div>
+  );
+};
+
+export default AddTaskModal;
