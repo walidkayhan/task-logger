a1979a32cebec763706bb5978e8d157cccc11cbc
Walid <walid.kayhan@mohawkcollege.ca>
2020-02-23 20:43:28 -0500

    Implemented main app component with react routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,32 @@
 import React from "react";
+import "antd/dist/antd.css";
 import "./App.css";
 
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import Header from "./layout/Header";
+import Footer from "./layout/Footer";
+
+import Tasks from "./components/Tasks";
+import Users from "./components/Users";
+import About from "./components/About";
+
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Header />
+
+      <div className="container">
+        <Switch>
+          <Route exact path="/" component={Tasks}></Route>
+          <Route exact path="/about" component={About}></Route>
+          <Route exact path="/users" component={Users}></Route>
+        </Switch>
+      </div>
+
+      <Footer />
+    </Router>
+  );
 };
 
 export default App;
