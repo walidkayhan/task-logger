ae0fcb8d2b940d5261ffca99ef43547e4d8c5c74
Walid <walid.kayhan@mohawkcollege.ca>
2020-02-23 23:46:57 -0500

    Table and data moved to seperate component

---
+++ client/src/components/TasksTable.js
@@ -0,0 +1,101 @@
+import React, { useState } from "react";
+import { Table, Avatar } from "antd";
+
+const TasksTable = () => {
+  const columns = [
+    {
+      title: "Task",
+      dataIndex: "task",
+      key: "task"
+    },
+    {
+      title: "Type",
+      dataIndex: "type",
+      key: "type"
+    },
+    {
+      title: "Description",
+      dataIndex: "description",
+      key: "description",
+      render: text => (text ? text : "None")
+    },
+    {
+      title: "User",
+      dataIndex: "user",
+      key: "user",
+      render: data => (
+        <div>
+          <Avatar style={{ backgroundColor: data.favoriteColor }}>
+            {data.name.charAt(0)}
+          </Avatar>{" "}
+          {data.name}
+        </div>
+      )
+    },
+    {
+      title: "Start Date",
+      dataIndex: "startDate",
+      key: "startDate"
+    },
+    {
+      title: "End Date",
+      dataIndex: "endDate",
+      key: "endDate"
+    }
+  ];
+
+  const data = [
+    {
+      key: "1",
+      task: "Updated UI",
+      type: "Information Technology",
+      description: "Updated UI of our website to a more modern look",
+      user: {
+        id: "njqhwudg7sa",
+        name: "John Appleseed",
+        favoriteColor: "#f56a00"
+      },
+      startDate: "2020-02-17",
+      endDate: "2020-02-24"
+    },
+    {
+      key: "2",
+      task: "Collected 2 Account Receivables",
+      type: "Financial",
+      description: "Collected 2 payments for outstanding invoices",
+      user: {
+        id: "hjdsuiasgd783",
+        name: "Pamela Smith",
+        favoriteColor: "#7cb305"
+      },
+      startDate: "2020-02-23",
+      endDate: "2020-02-23"
+    },
+    {
+      key: "3",
+      task: "Developed Advertisment Banner Wireframe",
+      type: "Marketing",
+      description: "",
+      user: {
+        id: "dnaduasy7321",
+        name: "Mike Wazowski",
+        favoriteColor: "#faad14"
+      },
+      startDate: "2020-02-17",
+      endDate: "2020-02-24"
+    }
+  ];
+  const [selectedRows, setSelectedRows] = useState([]);
+
+  const onSelectChange = selectedRowKeys => setSelectedRows(selectedRowKeys);
+  const rowSelection = {
+    selectedRows,
+    onChange: onSelectChange
+  };
+
+  return (
+    <Table columns={columns} dataSource={data} rowSelection={rowSelection} />
+  );
+};
+
+export default TasksTable;
