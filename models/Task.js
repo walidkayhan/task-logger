7e50c45327f3bb5368ad83997c866f6d5a552038
Walid <walid.kayhan@mohawkcollege.ca>
2020-03-08 01:15:11 -0500

    Adjusted schema naming and value types

--- models/Task.js
+++ models/Task.js
@@ -3,9 +3,13 @@ const Schema = mongoose.Schema;
 
 const TaskSchema = new Schema({
   title: { type: String, required: true },
-  type: { type: String, default: null },
-  additionalInformation: { type: String, default: null },
-  user: { type: mongoose.ObjectId, required: true },
+  department: { type: String, default: null },
+  description: { type: String, default: null },
+  user: {
+    _id: { type: mongoose.ObjectId, required: true },
+    name: { type: String, required: true },
+    favoriteColor: { type: String, default: "#00000" }
+  },
   startDate: { type: Date, default: null },
   endDate: { type: Date, default: null },
   created: { type: Date, default: Date.now }
--- models/User.js
+++ models/User.js
@@ -7,6 +7,7 @@ const UserSchema = new Schema({
   lastName: { type: String, required: true },
   email: { type: String, required: true },
   position: { type: String, required: true },
+  department: { type: String, required: true },
   favoriteColor: { type: String, required: true },
   created: { type: Date, default: Date.now }
 });
