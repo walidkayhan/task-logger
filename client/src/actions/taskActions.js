70dae8df8cdeeb1c5a1d86af804a23446d6c163a
Walid <walid.kayhan@mohawkcollege.ca>
2020-03-19 20:10:29 -0400

    Added new actions and adjusted others

--- client/src/actions/taskActions.js
+++ client/src/actions/taskActions.js
@@ -4,14 +4,15 @@ import {
   EDIT_TASK,
   DELETE_TASKS,
   SET_CURRENT_TASK,
+  SELECT_CURRENT_TASK,
   SELECT_TASK,
-  SELECT_TASKS,
+  SELECT_ALL_TASKS,
   UNSELECT_TASK,
   UNSELECT_ALL_TASKS,
   CLEAR_CURRENT_TASK,
-  OPEN_ADD_TASK_MODAL,
-  CLOSE_ADD_TASK_MODAL,
-  SET_LOADING,
+  OPEN_TASK_MODAL,
+  CLOSE_TASK_MODAL,
+  SET_TASK_LOADING,
   HANDLE_ERRORS,
   CLEAR_ERRORS
 } from "../store/Types";
@@ -36,8 +37,6 @@ const errorOutput = (error, dispatch) => {
         "There seems to be a problem with the server right now, please try again later"
     });
   } else {
-    dispatch({ type: HANDLE_ERRORS });
-
     if (error.response.data.errors) {
       const { errors } = error.response.data;
 
@@ -57,17 +56,18 @@ const errorOutput = (error, dispatch) => {
   }
 };
 
-export const openModal = () => dispatch => {
-  dispatch({ type: OPEN_ADD_TASK_MODAL });
+export const openModal = (type, id) => dispatch => {
+  dispatch({ type: OPEN_TASK_MODAL, payload: type });
+  id && dispatch({ type: SELECT_CURRENT_TASK, payload: id });
 };
 
 export const closeModal = () => dispatch => {
-  dispatch({ type: CLOSE_ADD_TASK_MODAL });
+  dispatch({ type: CLOSE_TASK_MODAL });
 };
 
 export const getTasks = () => async dispatch => {
   try {
-    dispatch({ type: SET_LOADING });
+    dispatch({ type: SET_TASK_LOADING });
 
     const res = await axios.get("/api/tasks", null, config);
 
@@ -80,7 +80,7 @@ export const getTasks = () => async dispatch => {
 
 export const addTask = task => async dispatch => {
   try {
-    dispatch({ type: SET_LOADING });
+    dispatch({ type: SET_TASK_LOADING });
 
     const res = await axios.post("/api/tasks", task, config);
 
@@ -95,10 +95,9 @@ export const addTask = task => async dispatch => {
   }
 };
 
-//NOT FINISHED, SOME PROBLEMS IN THIS
 export const editTask = task => async dispatch => {
   try {
-    const res = await axios.put(`/api/tasks/${task._id}`, config);
+    const res = await axios.put(`/api/tasks/${task._id}`, task, config);
 
     if (res.data.success) {
       dispatch({ type: EDIT_TASK, payload: task });
@@ -111,13 +110,14 @@ export const editTask = task => async dispatch => {
   }
 };
 
-//NOT FINISHED, SOME PROBLEMS IN THIS
-export const deleteTask = task => async dispatch => {
+export const deleteTasks = tasksList => async dispatch => {
   try {
-    const res = await axios.delete(`/api/tasks/${task._id}`, config);
+    dispatch({ type: SET_TASK_LOADING });
+
+    const res = await axios.post("/api/tasks/delete", { tasksList }, config);
 
     if (res.data.success) {
-      dispatch({ type: DELETE_TASKS, payload: task });
+      dispatch({ type: DELETE_TASKS, payload: tasksList });
       message.success(res.data.message);
     } else {
       message.error(res.data.message);
@@ -127,6 +127,27 @@ export const deleteTask = task => async dispatch => {
   }
 };
 
+export const selectTask = id => dispatch => {
+  dispatch({ type: SELECT_TASK, payload: id });
+};
+
+export const unselectTask = id => dispatch =>
+  dispatch({ type: UNSELECT_TASK, payload: id });
+
+export const selectAllTasks = () => dispatch =>
+  dispatch({ type: SELECT_ALL_TASKS });
+
+export const unselectAllTasks = () => dispatch =>
+  dispatch({ type: UNSELECT_ALL_TASKS });
+
 export const setCurrentTask = task => dispatch => {
   dispatch({ type: SET_CURRENT_TASK, payload: task });
+};
+
+export const setErrors = error => dispatch => {
+  dispatch({ type: HANDLE_ERRORS, payload: error });
+};
+
+export const clearErrors = () => dispatch => {
+  dispatch({ type: CLEAR_ERRORS });
 };
